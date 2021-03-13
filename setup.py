d7d6ff6761408d26fcef956b0a13c14f4ac452f3
spamz23 <diogo_silva30@hotmail.com>
2021-03-13 14:24:23 +0000

    :gem: Version 0.0.8
    - Fix Documentation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wagtail-social-share",
-    version="0.0.6",
+    version="0.0.8",
     author="Diogo Silva",
     author_email="diogosilv30@gmail.com",
     description="Auto-share Wagtail Pages on Social Medias",
