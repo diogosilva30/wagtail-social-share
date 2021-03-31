4a33786098d83457a520bb4952109bb24b5d47b1
spamz23 <diogo_silva30@hotmail.com>
2021-03-31 22:39:40 +0100

    :gem: Version 0.1.2 Release Notes: - Add Broader Wagtail Versions to 'requirements.txt'

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="wagtail-social-share",
-    version="0.1.1",
+    version="0.1.2",
     author="Diogo Silva",
     author_email="diogosilv30@gmail.com",
     description="Auto-share Wagtail Pages on Social Medias",
